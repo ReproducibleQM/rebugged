ac10e55e5ec2402e81c8e0f00dbe67d063f40740
Tim Head <betatim@gmail.com>
2018-09-12 13:58:37 +0200

    Create install.R

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages("lubridate")
+install.packages("reshape2")
+install.packages("plyr")
+install.packages("ggplot2")
+install.packages("gridExtra")
+install.packages("vegan")
+install.packages("MASS")
